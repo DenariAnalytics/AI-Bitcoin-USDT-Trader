63c992da17bcda4b2c1f8491b9c777a16aa69463
DenariAnalytics <fid_kk@proton.me>
2023-04-13 11:12:40 +0100

    bug fix

--- trade.py
+++ trade.py
@@ -4,23 +4,17 @@ import config
 api_key = config.api_key
 api_secret = config.api_secret
 
-binance = ccxt.binance({
+exchange = ccxt.binance({
     'apiKey': api_key,
     'secret': api_secret,
 })
 
 def get_balance(symbol):
     symbol = symbol.upper()
-    balance = binance.fetch_balance()
+    balance = exchange.fetch_balance()
     return balance[symbol]['free']
 
 def get_price(symbol):
-    """
-    Get the current price of a trading pair.
-
-    :param symbol: The trading pair symbol, e.g. 'BTC/USDT'
-    :return: The current price as a float
-    """
     ticker = exchange.fetch_ticker(symbol)
     return float(ticker['ask'])
 
@@ -34,9 +28,9 @@ def execute_trade(symbol, trade_type, amount):
 
     try:
         if trade_type == 'buy':
-            order = binance.create_market_buy_order(symbol, amount)
+            order = exchange.create_market_buy_order(symbol, amount)
         elif trade_type == 'sell':
-            order = binance.create_market_sell_order(symbol, amount)
+            order = exchange.create_market_sell_order(symbol, amount)
         elif trade_type == 'hold':
             order = 'Holding'
         if trade_type != 'hold':
